4fbd0bc8d85467bbbc34aeb1a65e3978d2be6559
Davidhzk2 <dherrera035@misena.edu.co>
2021-05-26 16:18:27 -0500

    Segunda linea del teclado funcionando

--- keyboard.js
+++ keyboard.js
@@ -5,6 +5,8 @@ let textBox = document.getElementById("textBox");
 
 let btnBackspace = document.getElementById("btn-Backspace");
 let btnSpace = document.getElementById("btn-space");
+let btnTab = document.getElementById("btn-tab");
+let btnIntro = document.getElementById("btn-intro");
 
 //  caracters
 let btnPipe = document.getElementById("btnPipe");
@@ -21,6 +23,20 @@ let btn0 = document.getElementById("btn-zero");
 let btnqo = document.getElementById("btn-qo");
 let btnqc = document.getElementById("btn-qc");
 
+let btnq = document.getElementById("btn-q");
+let btnw = document.getElementById("btn-w");
+let btne = document.getElementById("btn-e");
+let btnr = document.getElementById("btn-r");
+let btnt = document.getElementById("btn-t");
+let btny = document.getElementById("btn-y");
+let btnu = document.getElementById("btn-u");
+let btni = document.getElementById("btn-i");
+let btno = document.getElementById("btn-o");
+let btnp = document.getElementById("btn-p");
+let btnAcent = document.getElementById("btn-acent");
+let btnPlus = document.getElementById("btn-plus");
+// let btn = document.getElementById("btn-q");
+
 
 
 // funciones
@@ -35,66 +51,105 @@ const space =() =>{
     textBox.innerHTML = textBox.innerHTML + " "; 
 }
 
+const tab = () =>{
+    textBox.innerHTML = textBox.innerHTML + "    ";
+}
+const intro = () =>{
+    textBox.innerHTML = textBox.innerHTML + "\n";
+}
+
 
 
 // Eventos
 
 btnPipe.onclick = () =>{
-    // console.log("|");
     writeInBox(btnPipe.innerHTML);
 }
 btn1.onclick = () =>{
-    // console.log("|");
     writeInBox(btn1.innerHTML);
 }
 btn2.onclick = () =>{
-    // console.log("|");
     writeInBox(btn2.innerHTML);
 }
 btn3.onclick = () =>{
-    // console.log("|");
     writeInBox(btn3.innerHTML);
 }
 btn4.onclick = () =>{
-    // console.log("|");
     writeInBox(btn4.innerHTML);
 }
 btn5.onclick = () =>{
-    // console.log("|");
     writeInBox(btn5.innerHTML);
 }
 btn6.onclick = () =>{
-    // console.log("|");
     writeInBox(btn6.innerHTML);
 }
 btn7.onclick = () =>{
-    // console.log("|");
     writeInBox(btn7.innerHTML);
 }
 btn8.onclick = () =>{
-    // console.log("|");
     writeInBox(btn8.innerHTML);
 }
 btn9.onclick = () =>{
-    // console.log("|");
     writeInBox(btn9.innerHTML);
 }
 btn0.onclick = () =>{
-    // console.log("|");
     writeInBox(btn0.innerHTML);
 }
 btnqo.onclick = () =>{
-    // console.log("|");
     writeInBox(btnqo.innerHTML);
 }
 btnqc.onclick = () =>{
-    // console.log("|");
     writeInBox(btnqc.innerHTML);
 }
+btnq.onclick = () =>{
+    writeInBox(btnq.innerHTML);
+}
+btnw.onclick = () =>{
+    writeInBox(btnw.innerHTML);
+}
+btne.onclick = () =>{
+    writeInBox(btne.innerHTML);
+}
+btnr.onclick = () =>{
+    writeInBox(btnr.innerHTML);
+}
+btnt.onclick = () =>{
+    writeInBox(btnt.innerHTML);
+}
+btny.onclick = () =>{
+    writeInBox(btny.innerHTML);
+}
+btnu.onclick = () =>{
+    writeInBox(btnu.innerHTML);
+}
+btni.onclick = () =>{
+    writeInBox(btni.innerHTML);
+}
+btno.onclick = () =>{
+    writeInBox(btno.innerHTML);
+}
+btnp.onclick = () =>{
+    writeInBox(btnp.innerHTML);
+}
+btnAcent.onclick = () =>{
+    writeInBox(btnAcent.innerHTML);
+}
+btnPlus.onclick = () =>{
+    writeInBox(btnPlus.innerHTML);
+}
+
+
+// Special Buttons
 
 btnBackspace.onclick =() =>{
     backspace();
 };
 btnSpace.onclick = () =>{
     space();
+}
+btnTab.onclick = () =>{
+    tab();
+}
+btnIntro.onclick = () =>{
+    intro();
 }
